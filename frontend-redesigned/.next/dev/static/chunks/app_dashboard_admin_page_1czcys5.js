f7d2e10d455b3df5567692ffe4af465c334df51e
Danyaal19 <mohammeddanyaalshaikh@gmail.com>
2026-07-11 11:37:50 +0530

    Remove frontend-redesigned folder

--- frontend-redesigned/.next/dev/static/chunks/app_dashboard_admin_page_1czcys5.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_dashboard_admin_page_1qlm0h3.js"],
-    source: "dynamic"
-});
